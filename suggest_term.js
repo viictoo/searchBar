61cb852d2218bfb24f76e9d638e229beeb249974
Victor Langat <provicml@gmail.com>
2024-01-22 14:26:08 +0300

    search item, search all, aggregate and sugggest added

---
+++ suggest_term.js
@@ -0,0 +1,45 @@
+(function () {
+  'use strict';
+
+  const elasticsearch = require('elasticsearch');
+  const esClient = new elasticsearch.Client({
+    host: '127.0.0.1:9200',
+    log: 'error'
+  });
+
+  const suggest = function search(index, body) {
+    return esClient.search({ index: index, body: body });
+  };
+
+  // only for testing purposes
+  // all calls should be initiated through the module
+  const test = function test() {
+    let body = {
+      suggest: {
+        titleSuggester: {
+          text: "dolo lore fugi",
+          term: { field: "title" }
+        }
+      }
+    };
+
+    console.log(`retrieving term suggestions for "${body.text}"...`);
+    suggest('library', body)
+      .then(results => {
+        console.log(results);
+        console.log(`suggestions for each term are:`);
+        results.suggest.titleSuggester.forEach((term, index) => {
+          console.log(`term ${++index}: ${term.text}`);
+          term.options.forEach((option, index) => console.log(`\t suggestion ${++index}: ${option.text}`));
+        });
+      })
+      .catch(console.error);
+  };
+
+  test();
+
+  module.exports = {
+    suggest
+  };
+  //fielddata=true
+}());
